2c7d8c8ace2ea8c182fbf625e68ac3131b993e26
Kyle McKee <kmckee@itscovered.com>
2024-08-05 08:23:26 -0400

    created genserver

---
+++ hangman/lib/runtime/server.ex
@@ -0,0 +1,29 @@
+defmodule Hangman.Runtime.Server do
+  alias Hangman.Impl.Game
+
+  use GenServer
+
+  @me __MODULE__
+
+  ### Used by Client Process
+  def start_link do
+    GenServer.start_link(@me, nil)
+  end
+
+  ### Used by Server Process
+
+  def init(_) do
+    game = Game.new_game()
+    {:ok, game}
+  end
+
+  def handle_call({:make_move, guess}, _from, game) do
+    {updated_game, tally} = Game.make_move(game, guess)
+    {:reply, tally, updated_game}
+  end
+
+  def handle_call({:tally}, _from, game) do
+    tally = Game.tally(game)
+    {:reply, tally, game}
+  end
+end
